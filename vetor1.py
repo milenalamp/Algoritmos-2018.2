d92577d5e2a2be234d861c4756a8fd0cffd1a382
milenalamp <31291986+milenalamp@users.noreply.github.com>
2018-05-09 22:03:22 -0300

    Lista 1 vetores

---
+++ vetor1.py
@@ -0,0 +1,5 @@
+
+
+mes = [ "Janeiro","fevereiro","março","abril","maio","junho","julho","agosto","setembro","outubro","novembro","dezembro"]
+num = int(input("Digite um número: "))
+print(mes[num-1])
